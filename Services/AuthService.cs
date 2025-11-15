8c6606ff2d92cdb01701ff46990b08c5fccfe32c
Islam Fathy <30509167+islamf3thy@users.noreply.github.com>
2025-11-15 14:32:54 +0300

    Add files via upload

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,1 +1,40 @@
+using Microsoft.IdentityModel.Tokens;
+using SafeVaultSecurityProject.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
+namespace SafeVaultSecurityProject.Services
+{
+    public class AuthService
+    {
+        private readonly IConfiguration _config;
+
+        public AuthService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GenerateJwtToken(User user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
