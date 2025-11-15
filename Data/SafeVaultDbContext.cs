f185d9386092f8a50ffdb7cb609ca33c922630e7
Islam Fathy <30509167+islamf3thy@users.noreply.github.com>
2025-11-15 14:30:37 +0300

    Add files via upload

--- Data/SafeVaultDbContext.cs
+++ Data/SafeVaultDbContext.cs
@@ -1,1 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SafeVaultSecurityProject.Models;
 
+namespace SafeVaultSecurityProject.Data
+{
+    public class SafeVaultDbContext : DbContext
+    {
+        public SafeVaultDbContext(DbContextOptions<SafeVaultDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
